91103aa651ba358be89d3309127ed3df70a47551
eanplatter <eanplatter@gmail.com>
2014-10-07 16:38:57 +0000

    omg

--- src/scope.js
+++ src/scope.js
@@ -1,31 +1,30 @@
 /* jshint globalstrict: true */
+
 'use strict';
-function initWatchVal(){}
 
 function Scope() {
-  this.$$watchers = [];
+	this.$$watchers = [];
 }
 
-Scope.prototype.$watch = function(watcherFn, listenerFn){
-  var watcher = {
-    watcherFn: watcherFn,
-    listenerFn: listenerFn,
-    last: initWatchVal
-  };
-
-  this.$$watchers.push(watcher);
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+	var watcher = {
+		watchFn: watchFn,
+		listenerFn: listenerFn
+	};
+	this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function(){
-  var self = this;
-  var newValue;
-  var oldValue;
-  _.forEach(this.$$watchers, function(watcher){
-    newValue = watcher.watcherFn(self);
-    oldValue = watcher.last;
-    if(newValue !== oldValue){
-      watcher.last = newValue;
-      watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue),self);
-    }
-  });
+Scope.prototype.$digest = function() {
+	var self = this;
+	var newValue; 
+	var oldValue;
+	_.forEach(this.$$watchers, function(watcher) {
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+
+		if(newValue !== oldValue) {
+			watcher.last = newValue;
+			watcher.listenerFn(newValue, oldValue, self);
+		}
+	});
 };
